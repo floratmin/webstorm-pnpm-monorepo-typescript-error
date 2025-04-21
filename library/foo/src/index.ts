5a057ff4c73b05a3a956593236fbe2c538966fe2
Florian Koch <florat.min@gmail.com>
2025-04-21 12:13:29 +0200

    initial commit

---
+++ library/foo/src/index.ts
@@ -0,0 +1,9 @@
+export type Foo = {
+  a: string;
+  b: string;
+};
+
+export const foo: Foo = {
+  a: 'foo',
+  b: 'bar',
+};
---
+++ library/foo/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es'],
+    },
+  },
+  resolve: {
+    alias: {
+      src: resolve('src/'),
+    },
+  },
+  plugins: [
+    dts({tsconfigPath: 'tsconfig.json'}),
+  ],
+});
