8d6b3f6a3c7c2b82dc346f1e5e1b266ff81b749d
Florian Koch <florat.min@gmail.com>
2025-05-07 18:22:46 +0200

    added bax

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,7 +1,9 @@
 import {foo} from 'foo';
 import {bar} from 'bar/bar';
 import {baz} from 'baz/baz';
+import {bax} from 'bax';
 
 console.log(foo.a);
 console.log(bar.a);
 console.log(baz.a);
+console.log(bax.a);
---
+++ library/bax/src/index.ts
@@ -0,0 +1,9 @@
+export type Bax = {
+  a: string;
+  b: string;
+};
+
+export const bax: Bax = {
+  a: 'foo',
+  b: 'bar',
+};
